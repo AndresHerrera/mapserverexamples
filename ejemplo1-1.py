9a2a8799bdd1dabab39caee3f9c2a667a97eced9
Andres Herrera <t763rm3n@gmail.com>
2018-11-03 06:26:43 -0500

    mapserver examples

---
+++ ejemplo1-1.py
@@ -0,0 +1,10 @@
+import mapscript
+
+test_map = mapscript.mapObj("ejemplo1.map")
+
+p = mapscript.pointObj(1, 1)
+
+print(p)
+
+test_map.draw()
+
---
+++ ejemplo1.py
@@ -0,0 +1,4 @@
+import mapscript
+test_map = mapscript.mapObj(r"ejemplo1.map")
+e = test_map.extent
+print(e)
